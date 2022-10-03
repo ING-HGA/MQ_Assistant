03850dd169bc0ea03672c85a663471850227098f
ING-HGA <45300713+ING-HGA@users.noreply.github.com>
2022-10-02 22:12:55 -0500

    Add files via upload

--- MALDIquantReport.R
+++ MALDIquantReport.R
@@ -1,5 +1,5 @@
-source("~/Nextcloud/agraphia (3)/projects/Hector/Scrips R/MALDIquantReport/MALDIquantReport-Functions.R", encoding = 'UTF-8') # File path to "DLI-ESI-LCQ-Functions.R" file
+source("~/Nextcloud/agraphia (3)/projects/Hector/Scrips R/MALDIquantReport/MALDIquantReport-Functions.R", encoding = 'UTF-8') # File path to "MALDIquantReport-Functions.R" file
 source("~/Nextcloud/agraphia (3)/projects/Hector/Scrips R/MALDIquantReport/Auto-MALDIquantReport.R", encoding = 'UTF-8') # File path to "Autorun.R"
-setwd("~/Nextcloud/agraphia (3)/projects/Hector/Scrips R/MALDIquantReport/Dataset") # Working directory with spectra
+setwd("~/Nextcloud/agraphia (3)/projects/Hector/Scrips R/MALDIquantReport/Dataset") # Working directory and spectra
 
 featureMatrix <- autoMALDIquantReport() # Run automatically and save matrix and settings
