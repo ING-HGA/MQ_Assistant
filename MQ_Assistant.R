f80db9411390b50491a3832fc8a7d9d05cfaed01
ING-HGA <45300713+ING-HGA@users.noreply.github.com>
2023-02-05 22:23:14 -0600

    Add files via upload

---
+++ MQ_Assistant.R
@@ -0,0 +1,13 @@
+# Set working directory with spectra
+setwd("~/Nextcloud/agraphia (3)/projects/Hector/Scrips R/MQ_Assistant/Dataset")
+
+### Load file with functions:###
+
+# File path to "MQ_Assistant-Functions.R" file
+source("~/Nextcloud/agraphia (3)/projects/Hector/Scrips R/MQ_Assistant/MQ_Assistant-Functions.R", encoding = 'UTF-8') 
+# File path to "MQ_Assistant-Functions.R" file
+source("~/Nextcloud/agraphia (3)/projects/Hector/Scrips R/MQ_Assistant/Auto_MQ_Assistant.R", encoding = 'UTF-8')
+
+### Run ###
+# Execute MALDIquant workflow automatically and save data matrix and settings at the end
+featureMatrix <- autoMCAssistant()
